17cfd3aed209ab316cf2a52f6a82f31a614e7ff8
Jorge <jepers2021@gmail.com>
2021-03-26 21:48:34 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace A133590.Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Ejercicio 3.");
+            Console.WriteLine("El siguiente lee las teclas y finaliza al presionar la tecla \"x\".");
+            while (Console.ReadKey().Key != ConsoleKey.X)
+            {
+
+            }
+            Console.WriteLine("Presione cualquier tecla para salir..");
+            Console.ReadKey();
+        }
+    }
+}
